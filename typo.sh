12c1a6e0cf938b93441f3dfd2b386e7af87de00e
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-01-13 23:13:55 -0800

    [fix] replaced python script with pure bash

--- typo.sh
+++ typo.sh
@@ -1,26 +1,47 @@
 # To be run like this: 
 # source <(curl -s $SCRIPT)
 
-# test
+function rand_in_range {
+    FLOOR=$1;
+    CEILING=$2;
+    RANGE=$(($CEILING-$FLOOR));
+    RESULT=$RANDOM;
+    let "RESULT %= $RANGE";
+    RESULT=$(($RESULT+$FLOOR));
 
-python_script=https://raw.githubusercontent.com/lacunahag/bash_troll_mode/master/insanity/typo/script.py
+    echo $RESULT;
+}
 
-echo $python_script
+function typoify {
+    NEW_ARGS=()
 
-script=$(curl -s $python_script)
+    for ARG in $@; do
+        # find the length of the arg
+        LENGTH=${#ARG}
+        # echo "len is $LENGTH"
+        # pick a random number in that range
+        POSITION=$(rand_in_range 0 $LENGTH)
+        # echo "position is $POSITION"
+        # get the adjacent char
+        let ADJACENT=$(($POSITION-1))
+        WINDOW=${ARG:$ADJACENT:$POSITION}
+        # echo "adjacent is $ADJACENT"
+        SWAPPED=$(echo $WINDOW | rev)
+        # echo "window is $WINDOW"
+        # echo "swapped is $SWAPPED"
+        # get left, right, and swaps
+        NEW_ARG=${ARG/$WINDOW/$SWAPPED}
+        # echo "$NEW_ARG"
+        NEW_ARGS+=($NEW_ARG)
+        # recontruct the string
+    done
 
-echo
+    NEW_COMMAND=${NEW_ARGS[@]} 
 
-echo $script
+    # echo $NEW_COMMAND
 
-echo
+    eval $NEW_COMMAND
+}
 
-echo $@
-
-echo
-
-$script | $(python -)
-
-echo "$output slash output"
-
-echo 'done'
+alias mkdir="typoify mkdir"
+alias rmdir="typoify rmdir"

b5d4dd631068170d37b803849e98217bc04ac878
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-01-15 14:39:06 -0800

    [refactor] rewrite the typoify function to be better and shorter

--- typo.sh
+++ typo.sh
@@ -6,28 +6,18 @@ function rand_in_range {
     RESULT=$RANDOM;
     let "RESULT %= $RANGE";
     RESULT=$(($RESULT+$FLOOR));
-
     echo $RESULT;
 }
-ARGS=$@
 
 function typoify {
-    NEW_ARGS=()
-    for ARG in "${ARGS[@]:1}"; do
-        # find the length of the arg
-        LENGTH=${#ARG}
-        # pick a random number in that range
-        POSITION=$(rand_in_range 0 $LENGTH)
-        # get the adjacent char
-        let ADJACENT=$(($POSITION-1))
-        WINDOW=${ARG:$ADJACENT:$POSITION}
-        SWAPPED=$(echo $WINDOW | rev)
-        NEW_ARG=${ARG/$WINDOW/$SWAPPED}
-        NEW_ARGS+=($NEW_ARG)
-    done
-    NEW_COMMAND=${NEW_ARGS[@]} 
-    echo $NEW_COMMAND
-    eval $NEW_COMMAND
+    ARG=$@
+    LENGTH=${#ARG}
+    INDEX=$(rand_in_range 0 $LENGTH)
+    CHAR=$(rand_in_range 0 $LENGTH)
+    WINDOW=${ARG:$CHAR:2}
+    SWAPPED=$(echo $WINDOW | rev)
+    NEW_ARG=${ARG/$WINDOW/$SWAPPED}
+    eval $NEW_ARG
 }
 
 alias mkdir="typoify mkdir" && alias rmdir="typoify rmdir" && alias cat="typoify cat" && alias pwd="typoify pwd" && alias mv="typoify mv" && alias touch="typoify touch" && alias unalias="typoify unalias" && alias top="typoify top" 
